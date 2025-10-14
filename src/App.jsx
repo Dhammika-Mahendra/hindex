3c487f5203a345c76bcedc3819db922808fcdec0
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2025-10-14 17:42:36 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+
+function App() {
+
+  return (
+      <div className="relative flex">
+
+       <h1>hello</h1>
+
+      </div>
+  )
+}
+
+export default App
